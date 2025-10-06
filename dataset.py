886319512056cace0a63e3d3cd273965225bfd7d
Lohann-cv <lohanncoantverdier@gmail.com>
2025-10-06 16:44:10 +0200

    Archi menu

--- dataset.py
+++ dataset.py
@@ -10,12 +10,31 @@ def shape_info():
 
 def average_carat_price():
     choice = input(
-        "Quel valeur moyenne voulez vous ?\nCarat ou Prix? : ").lower
+        "Quel valeur moyenne voulez vous ?\nCarat ou Prix? : ").lower()
     if choice == "carat":
         average_carat = df["carat"]
         print(
             f"Le carat moyen des diamands de ce dataset est de : {average_carat.mean()}")
-    if choice == "prix":
+    elif choice == "prix":
         average_price = df["price"]
         print(
             f"Le prix moyen des diamands de ce dataset est de {average_price.mean()}")
+    else:
+        print("Veuiller rentrer 'carat' ou 'price' !")
+        return
+
+
+def max_min_price():
+    choice = input(
+        "Voulez vous savoir les informations du diamand le plus cher ou le moins cher? Choisisez Maximum ou Minimum : ").lower()
+    if choice == "maximum":
+        max_diamond = df["price"].max()
+        print(
+            f"Les informations du diamand le plus cher valant {max_diamond} sont :\n{df.loc[df['price'] == max_diamond]}")
+    elif choice == "minimum":
+        min_diamond = df["price"].min()
+        print(
+            f"Les informations du diamand le moins cher valant {min_diamond} sont :\n{df.loc[df['price'] == min_diamond]}")
+    else:
+        print("Veuillez rentrer 'maximum' ou 'minimum' !")
+        return
--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+def dataset_menu():
+    ed
+
+
+def ml_main():
+    ed
+
+
 def menu():
-    print()
+    while True:
+        print("Bonjour, choisisez ce que vous voulez faire aujourd'hui :\n1- Recherche du prix d'un diamand en fonction de ces carateristique\n2- Information relative à notre dataset\n3- Quiter")
+        choice = input(
+            "Veuillez rentrer la valeur numérique de votre option choisie : ")
+        if choice == "1":
+            ml_main()
+        elif choice == "2":
+            dataset_menu()
+        elif choice == "3":
+            print("Aurevoir et bonne journée !")
+            break
+        else:
+            print("Veuillez rentrer 1, 2 ou 3 selon votre option choisie !")
+            return
