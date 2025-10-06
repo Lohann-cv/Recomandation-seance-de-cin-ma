6825881f6175ee18a2f1364cfb8eea6bff38bb48
Lohann-cv <lohanncoantverdier@gmail.com>
2025-10-06 14:41:04 +0200

    fonction : shape et moyenne carat ou prix

--- dataset.py
+++ dataset.py
@@ -1,6 +1,21 @@
 import pandas as pd
 
 df = pd.read_csv("diamonds.csv")
-print(df.head(10))
 df = df[["carat", "cut", "color", "clarity", "price"]]
-print(df.head())
+
+
+def shape_info():
+    print(f"La shape du dataset utilisée est : {df.shape()}")
+
+
+def average_carat_price():
+    choice = input(
+        "Quel valeur moyenne voulez vous ?\nCarat ou Prix? : ").lower
+    if choice == "carat":
+        average_carat = df["carat"]
+        print(
+            f"Le carat moyen des diamands de ce dataset est de : {average_carat.mean()}")
+    if choice == "prix":
+        average_price = df["price"]
+        print(
+            f"Le prix moyen des diamands de ce dataset est de {average_price.mean()}")
