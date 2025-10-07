7314acb6c5261d1025284d45d2684672b21c2592
Lohann-cv <lohanncoantverdier@gmail.com>
2025-10-07 21:39:02 +0200

    Set up data frame et j'ai convertie les str en int

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,14 @@
-import sklearn as sk
+import sklearn
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import matplotlib as plt
+import numpy as np
+
+diamonds = pd.read_csv("diamonds.csv")
+print(diamonds.head())
+diamonds = diamonds[["carat", "cut", "color", "clarity", "price"]]
+print(diamonds.head(30))
+diamonds["cut"] = diamonds["cut"].rename(
+    ["Ideal", "Premium", "Very Good", "Good", "Fair"], ["1", "2", "3", "4", "5"])
+print(diamonds.head(30))
+model = KNeighborsClassifier()
